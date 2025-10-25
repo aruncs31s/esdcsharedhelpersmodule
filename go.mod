e6ee1d2a30b557531625127ff4d6a21cd31c4c65
Arun CS <aruncs31ss@gmail.com>
2025-10-25 15:52:22 +0530

    changed name

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module esdcsharedhelpers
+module esdcsharedhelpersmodule
 
 go 1.25.3
 
--- interface/helper/error_helper.go
+++ interface/helper/error_helper.go
@@ -1,5 +1,13 @@
 package helper
 
 type ErrorHelper interface {
+	// GetRecordDoesNotBelongErrorMessage constructs an error message indicating that a record does not belong to a specific user.
+	//
+	// Params:
+	// - id: any type representing the record ID.
+	// - user: string representing the username.
+	//
+	// Returns:
+	// - error: constructed error message.
 	GetRecordDoesNotBelongErrorMessage(id any, user string) error
 }
